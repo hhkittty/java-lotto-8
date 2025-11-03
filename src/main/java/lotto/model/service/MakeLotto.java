6ee986e2b2d44ffb0e7b34edf67c5dc4b9c2f821
hhkittty <hyenggg0406@gmail.com>
2025-11-03 14:18:08 +0900

    chore: Remove unused imports and commented code.

--- src/main/java/lotto/model/service/MakeLotto.java
+++ src/main/java/lotto/model/service/MakeLotto.java
@@ -1,16 +1,10 @@
 package lotto.model.service;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import lotto.model.domain.Lotto;
 
 public class MakeLotto {
     private static final int LOTTO_PRICE = 1000;
-    private static final int LOTTO_MIN_NUMBER = 1;
-    private static final int LOTTO_MAX_NUMBER = 45;
-    private static final int LOTTO_SIZE = 6;
     private final NumberGenerator generator;
 
     public MakeLotto(NumberGenerator generator) {
@@ -21,14 +15,6 @@ public class MakeLotto {
         int countLotto = price / LOTTO_PRICE;
         return countLotto;
     }
-    /*public List<Integer> randomLotto(){
-        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER,LOTTO_MAX_NUMBER,LOTTO_SIZE);
-        List<Integer>lottosort= new ArrayList<>(lotto);
-        lottosort.sort(Comparator.naturalOrder());
-
-        return lottosort;
-    }*/
-
     public List<List<Integer>> makeLotto(int countLotto) {
         List<List<Integer>> lottos = new ArrayList<>();
         for (int i=0; i<countLotto; i++) {
