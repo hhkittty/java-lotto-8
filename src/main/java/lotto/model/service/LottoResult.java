0f75abe62f64c3efece7867d9aebb4cdc6688e68
hhkittty <hyenggg0406@gmail.com>
2025-11-03 11:53:42 +0900

    feat: Implement LottoResult with DI and aggregation.

--- src/main/java/lotto/model/service/LottoResult.java
+++ src/main/java/lotto/model/service/LottoResult.java
@@ -1,46 +1,34 @@
-package lotto.model;
+package lotto.model.service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import lotto.model.domain.Rank;
+import lotto.model.domain.WinningNumbers;
 
 public class LottoResult {
-    private final List<Integer> lottoNumber;
-    private final int bonus;
-    private final List<List<Integer>> RandomLottos;
-    private final int inputPrice;
+    private final RankCalculator calculator;
 
-    public LottoResult(List<Integer> lottoNumber,int bonusNumber,List<List<Integer>> RandomLottos,int inputPrice){
-        this.lottoNumber= lottoNumber;
-        this.bonus=bonusNumber;
-        this.RandomLottos= RandomLottos;
-        this.inputPrice= inputPrice;
-    }
-    public int countSame( List<Integer> randomnum) {
-        int countSame = 0;
-        for (int i : randomnum) {
-            if (lottoNumber.contains(i)) {
-                countSame++;
-            }
-        }
-        return countSame;
-    }
-    public boolean checkBonus(int bonus, List<Integer> randomnum) {
-        return randomnum.contains(bonus);
+    public LottoResult(RankCalculator rankCalculator) {
+        this.calculator = rankCalculator;
     }
 
-    public List<Integer> calculateRank(){
-        List<Integer> rankCount=new ArrayList<>(Arrays.asList(0,0,0,0,0,0));
-        for(List<Integer> lotto: RandomLottos) {
-            int countSame= countSame(lotto);
-            boolean checkBonus=checkBonus(bonus,lotto);
+    public List<Integer> calculateRank(WinningNumbers winning, List<List<Integer>> RandomLottos) {
+        List<Integer> rankCount = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0));
+        List<Integer> winNums = winning.getWinningNumbers();
+        int bonusNum = winning.getBonusNumber();
+
+        for (List<Integer> lotto : RandomLottos) {
+            int countSame = calculator.countSame(winNums,lotto);
+            boolean checkBonus = calculator.checkBonus(bonusNum, lotto);
 
             Rank rank = Rank.findRank(countSame, checkBonus);
-            rankCount.set(rank.ordinal(),rankCount.get(rank.ordinal())+1);
+            rankCount.set(rank.ordinal(), rankCount.get(rank.ordinal()) + 1);
         }
         return rankCount;
     }
-    public int totalPrice(List<Integer> totalCountCorrect){
+
+    public int totalPrize(List<Integer> totalCountCorrect){
         int sum=0;
         Rank[] ranks=Rank.values();
         for(int i=0;i<ranks.length;i++){
@@ -48,7 +36,7 @@ public class LottoResult {
         }
         return sum;
     }
-    public double pnl(int totalPrice){
+    public double pnl(int totalPrice,int inputPrice){
         double pnl = totalPrice/(double)inputPrice*100;
         pnl=Math.round(pnl*100.0)/100.0;
         return pnl;
