89923596d038e83465d186fe0e774578e622fb70
hhkittty <hyenggg0406@gmail.com>
2025-11-03 11:38:54 +0900

    feat: Define WinningNumbers domain with validation.

--- src/main/java/lotto/model/domain/WinningNumbers.java
+++ src/main/java/lotto/model/domain/WinningNumbers.java
@@ -1,4 +1,21 @@
 package lotto.model.domain;
 
+import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
 public class WinningNumbers {
+    private final List<Integer> winningNumbers;
+    private final int bonus;
+
+    public WinningNumbers(List<Integer> winningNumbers, int bonus) {
+        this.winningNumbers=List.copyOf(winningNumbers);
+        this.bonus=bonus;
+    }
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+    public int getBonusNumber() {
+        return bonus;
+    }
 }
