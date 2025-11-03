39ea9cca5f117d30b3d2f46a06f241cd4e3b50dc
hhkittty <hyenggg0406@gmail.com>
2025-11-03 11:52:27 +0900

    feat: Implement RankCalculator service.

--- src/main/java/lotto/model/service/RankCalculator.java
+++ src/main/java/lotto/model/service/RankCalculator.java
@@ -1,4 +1,24 @@
 package lotto.model.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import lotto.model.domain.Rank;
+
 public class RankCalculator {
+    public int countSame(List<Integer> lottoNumber,List<Integer> randomnum) {
+        int countSame = 0;
+        for (int i : randomnum) {
+            if (lottoNumber.contains(i)) {
+                countSame++;
+            }
+        }
+        return countSame;
+    }
+
+    public boolean checkBonus(int bonus, List<Integer> randomnum) {
+        return randomnum.contains(bonus);
+    }
+
+
 }
