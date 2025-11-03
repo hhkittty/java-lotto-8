0387408d1e46ebd7d74cd5ffa15b39bb5a297519
hhkittty <hyenggg0406@gmail.com>
2025-11-03 11:51:20 +0900

    feat: Implement LottoNumberGenerator logic.

--- src/main/java/lotto/model/service/NumberGenerator.java
+++ src/main/java/lotto/model/service/NumberGenerator.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.model.service;
 
 import java.util.List;
 
--- src/main/java/lotto/model/service/RandomNumberGenerator.java
+++ src/main/java/lotto/model/service/RandomNumberGenerator.java
@@ -1,4 +1,21 @@
 package lotto.model.service;
 
-public class RandomNumberGenerator {
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class RandomNumberGenerator implements NumberGenerator {
+    private static final int LOTTO_MIN_NUMBER=1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+
+    @Override
+    public List<Integer> generate() {
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER,LOTTO_MAX_NUMBER,LOTTO_SIZE);
+        List<Integer>lottosort= new ArrayList<>(lotto);
+        lottosort.sort(Comparator.naturalOrder());
+
+        return lottosort;
+    }
 }
